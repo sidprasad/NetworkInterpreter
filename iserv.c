d3496f78aae03b180518ea7109cdf1515ff1c9ff
Siddhartha Prasad <Siddhartha.Prasad@tufts.edu>
2015-04-24 09:42:23 -0400

    Updated readme, did some cleanup

--- iserv.c
+++ iserv.c
@@ -251,9 +251,7 @@ int main(int argc, char *argv[])
         bzero((char*)connectlist, sizeof(connectlist));
 
         /* We set up a log of all commands sent as a file */
-
         ilog = fopen("logfile.scm", "w");
-
         /****************************************************/
         /* Now loop forever */ 
         while(1) {
@@ -364,14 +362,14 @@ void service_client(int index) {
             if (FILE_SEND(connectlist[index], interpreter_address) == 0) {
                 ack.type = htons(7);
                 write(connectlist[index], &ack, hsize);
-                fclose(ilog); 
+               // fclose(ilog); 
                 if (FILE_SEND(connectlist[index], "logfile.scm") == 0) {}
                 else  printf("transferring log file failed\n");
             } else printf("transferring intepreter file failed\n"); 
             close(connectlist[index]);
             connectlist[index] = 0;
-            ilog = fopen("logfile.scm", "w");
-            fseek(ilog, 0, SEEK_END); 
+            //ilog = fopen("logfile.scm", "w");
+            //fseek(ilog, 0, SEEK_END); 
         } else {
             fprintf(stderr, "Unrecognized type %hu\n", hd.type);
             c_error(index);
---
+++ logfile.scm
@@ -0,0 +1,14 @@
+(+ 6 7)
+
+(define square (x) (* x x))
+
+(square 50)
+
+(define square (x) (x))
+1
+(square 60)
+
+(define square (x) x)
+
+(square 30)
+
